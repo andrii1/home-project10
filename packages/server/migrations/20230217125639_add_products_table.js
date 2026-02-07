bbf20aa864100884362175ede2d4283196d31fa9
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2026-02-07 16:17:08 +0100

    wip

--- packages/server/migrations/20230217125639_add_products_table.js
+++ packages/server/migrations/20230217125639_add_products_table.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('products', (table) => {
     table.increments();
-    table.string('external_id', 100).nullable();
+    table.string('asin', 100).nullable();
     table.text('title').notNullable();
     table.string('slug').notNullable();
 
@@ -10,37 +10,32 @@ exports.up = function (knex) {
     table.integer('category_id').unsigned();
     table.foreign('category_id').references('id').inTable('categories');
 
-    table.integer('platform_id').unsigned();
-    table.foreign('platform_id').references('id').inTable('platforms');
-
     table.text('url').nullable();
     table.text('url_affiliate').nullable();
     table.text('url_image').nullable();
     table.string('url_icon').nullable();
-
+    table.string('url_serpapi').nullable();
+    table.string('options').nullable();
     table.string('brand').nullable();
-
     table.decimal('rating', 2, 1).unsigned().nullable();
     table.integer('reviews').unsigned().nullable();
-
     table.decimal('price', 10, 2).unsigned().nullable();
     table.decimal('price_original', 10, 2).unsigned().nullable();
     table.integer('discount_percentage').unsigned().nullable();
-
+    table.string('currency', 10).defaultTo('USD');
     table.boolean('is_prime').defaultTo(false);
-    table.boolean('trending').defaultTo(false);
+    table.boolean('featured').defaultTo(false);
+    table.boolean('sponsored').defaultTo(false);
+    table.boolean('top_rated').defaultTo(false);
 
     table.integer('bought_last_month').unsigned().nullable();
     table.integer('rank').unsigned().nullable(); // Movers & Shakers / Best Sellers rank
 
-    table.string('amazon_category').nullable();
-    table.string('currency', 10).defaultTo('USD');
     table.string('source').nullable();
 
     table.json('raw_data').nullable(); // Full SERP API dump
 
     table.string('meta_description').nullable();
-    table.json('raw_data').nullable();
 
     table.datetime('last_checked_at').nullable();
     table
@@ -49,7 +44,6 @@ exports.up = function (knex) {
 
     table.datetime('created_at', { precision: 6 }).defaultTo(knex.fn.now(6));
     table.datetime('updated_at', { precision: 6 }).defaultTo(knex.fn.now(6));
-    table.unique(['platform_id', 'external_id']);
   });
 };
 
